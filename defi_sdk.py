5536434ce6311ec119bbbb0f7f44c15fc03d8dec
Old School <fam.b.boy2@gmail.com>
2021-06-21 00:16:07 +0700

    add buy / sell tokens ,get token balance

--- defi_sdk.py
+++ defi_sdk.py
@@ -20,7 +20,6 @@ ABI = {
 }
 
 class DeFiSDK:
-	
 
 	def __init__(self,rpc_url,wallet_addr,private_key):
 		self.setRPC(rpc_url)
@@ -35,6 +34,7 @@ class DeFiSDK:
 		self.senderAddress = wallet_addr
 		self.private_key = wallet_key
 
+
 	def getPrice(self,factory,pair):
 		factoryAddress = self.web3.toChecksumAddress(Contracts['AMM'][factory]['Factory'])
 		tokenA, tokenB = [self.web3.toChecksumAddress(Contracts['Tokens'][i]) for i in pair.split('/')]
@@ -42,45 +42,58 @@ class DeFiSDK:
 		contract = self.web3.eth.contract(address=factoryAddress, abi=ABI['FACTORY'])
 		pair_address = contract.functions.getPair(tokenA, tokenB).call()
 		tokensPair = self.web3.eth.contract(abi=ABI['PAIR'], address=pair_address)
-		
+		print(pair_address)
 		reserves = tokensPair.functions.getReserves().call()
 		price = reserves[1] / reserves[0]
 		return price
 
+
 	def swap(self,factory,tokenA,tokenB,value):
-		factoryAddress = self.web3.toChecksumAddress(Contracts['AMM'][factory]['Factory'])
+		factoryAddress = self.web3.toChecksumAddress(Contracts['AMM'][factory]['Router'])
 
 		contract = self.web3.eth.contract(address=factoryAddress, abi=ABI['ROUTER'])
 		nonce = self.web3.eth.get_transaction_count(self.senderAddress)
-
-		txn = contract.functions.swapExactETHForTokens(
-                    0,  # set to 0, or specify minimum amount of tokeny you want to receive - consider decimals!!!
+		
+		amountIn = self.web3.toWei(value, 'ether')
+		amount1 = contract.functions.getAmountsOut(
+		    amountIn, [tokenB, tokenA]).call()
+		amountOutMin = int(amount1[1] * 0.99) #slippage 1%
+		# minAmountPrint = self.web3.fromWei(amountOutMin, 'ether')
+		# print('Minimum recieved:', minAmountPrint)
+		txn = contract.functions.swapExactTokensForTokens(
+                    amountIn,
+					amountOutMin,
                     [tokenB, tokenA],
                     self.senderAddress,
                     (int(time.time()) + 10000)
                 ).buildTransaction({
                     "from": self.senderAddress,
                     "gas": 250000,
-                    "gasPrice": self.web3.toWei(value, "ether"),
-                    # This is the Token(BNB) amount you want to Swap from
-                  	"value": self.web3.toWei(0.01, "ether"),
+                    "gasPrice": self.web3.toWei("6", "gwei"),
+                  	"value": 0,
                     "nonce": nonce,
                 })
 		signed_txn = self.web3.eth.account.sign_transaction(txn, private_key=self.private_key)
 		tx_token = self.web3.eth.send_raw_transaction(signed_txn.rawTransaction)
 		return self.web3.toHex(tx_token)
 
+
 	def buy(self,factory,pair,value):
 		tokenA, tokenB = [self.web3.toChecksumAddress(Contracts['Tokens'][i]) for i in pair.split('/')]
 		return self.swap(factory,tokenA,tokenB,value)
+
 
 	def sell(self, factory, pair, value):
 		tokenB, tokenA = [self.web3.toChecksumAddress(Contracts['Tokens'][i]) for i in pair.split('/')]
 		return self.swap(factory, tokenA, tokenB, value)
 
-sdk = DeFiSDK('https://bsc-dataseed.binance.org/',
-              '',
-              '')
-price = sdk.getPrice('Twindex','DOP/BUSD')
-print(price)
-# print(sdk.buy('Twindex','DOP/BUSD',0.01))
+
+	def getBalance(self,token):
+		token_address = self.web3.toChecksumAddress(Contracts['Tokens'][token])
+		contract = self.web3.eth.contract(address = token_address,abi=ABI['ERC_20'])
+
+		address = self.web3.toChecksumAddress(self.senderAddress)
+		balance = contract.functions.balanceOf(address).call()
+		data = self.web3.fromWei(balance,"ether")
+		return data
+
---
+++ run.py
@@ -0,0 +1,19 @@
+from web3.providers import rpc
+from defi_sdk import DeFiSDK
+sdk = DeFiSDK(rpc_url='https://bsc-dataseed.binance.org/',
+              wallet_addr='',
+              private_key='')
+#Get price form Dex
+price = sdk.getPrice('Twindex', 'DOP/BUSD')
+print(price)
+
+#Buy Order
+buy_hsh = sdk.buy('Twindex', 'DOP/BUSD', 5)  # 5 is DOP to buy
+print(buy_hsh)  
+
+#Sell Order
+sell_hsh = sdk.sell('Twindex', 'DOP/BUSD', 5) # 5 is DOP to sell
+
+#Get Token Balance
+tokenBalance = sdk.getBalance('DOP')
+print("DOP: ",tokenBalance)
